4e29ff096e249d3ec186a09c9cd8e9a106b64a76
p <p@maple.(none)>
2011-04-09 23:05:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+A lua quine
+#############
+by pzel
+-------------
+
+I hereby release this code into the public domain.
+
---
+++ quine.lua
@@ -0,0 +1,2 @@
+y="print('y=' .. string.format('%c',0x22) ..  y .. string.format('%c%c',0x22,0xa) .. 'loadstring(y)()' )"
+loadstring(y)()
